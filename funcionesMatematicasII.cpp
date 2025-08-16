e3f6b21c1deff08351db9aee1b56c2878f8a94ae
Manuel F. Borrego S. <manuelf.borrego@gmail.com>
2025-08-16 08:00:22 -0400

    Modific. en 'funcionesMatematicasII.cpp'...

--- funcionesMatematicasII.cpp
+++ funcionesMatematicasII.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include <stdio.h>
 #include <cmath>
 
-int main() {
+#define PI 3.14159265
+
+int main()
+{
         double u = 3.1187;
         double v = 7.8778;
         double x = 8;
         double y = 17;
         double z;
+        double ang;
+        double sine;
 
         // z = std::max(x, y); // ----> El mayor de 2 números...
         // z = std::min(x, y); // ----> El menor de 2 números...
@@ -16,9 +22,17 @@ int main() {
         // z = std::round(u); // ----> Redondea el valor al siguiente más cercano (bien hacia arriba o hacia abajo...)
         // z = std::round(v); // ----> Redondea el valor al siguiente más cercano (bien hacia arriba o hacia abajo...)
         // z = std::ceil(u);  // ----> Redondea el valor al más próximo (hacia arriba)...
-        z = std::floor(v); // ----> Redondea el valor al más próximo (hacia abjo)...
+        // z = std::floor(v); // ----> Redondea el valor al más próximo (hacia abjo)...
 
-        std::cout << z;
+        // std::cout << z;
+
+        std::cout << "Introduzca el valor del angulo para calcular su 'seno': ";
+        std::cin >> ang;
+
+        sine = sin(ang * PI / 180);
+
+        // std::cout << '\n' << "El seno de %f" << ang << " = %f" << seno;
+        printf("El seno of %f grados es %f.\n", ang, sine);
 
         return 0;
 }

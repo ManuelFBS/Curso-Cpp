80d8fd9ee088dc8c1d3e8fb95d405c5e03848fd6
ManuelFBS <manuelf.borrego@gmail.com>
2025-08-18 23:50:55 -0400

    Creac. de 'operadorTernario.cpp'...

---
+++ operadorTernario.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+
+int main() {
+        // int score = 85;
+
+        // score >= 60 ? printf("Aprobaste") : printf("Reprobaste");
+
+        // -------------------------------------------------------------------------------------------------------------------------
+
+        // int number = 10;
+
+        // number % 2 ? printf("El nro %d es impar !", number) : printf("El nro %d es par !", number);
+
+        // -------------------------------------------------------------------------------------------------------------------------
+
+        bool hambriento = false;
+
+        hambriento ? printf("Tienes mucha hambre...!") : printf("Estas full...!");
+
+        return 0;
+}

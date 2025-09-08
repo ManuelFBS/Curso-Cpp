5ecb3ad3e8f13ee04357089a5f20ba5eee408fd7
ManuelFBS <manuelf.borrego@gmail.com>
2025-09-08 07:09:15 -0400

    Creac. de 'phys_constants.hpp', 'manyCalcs.cpp'; Modif. de 'math_constants.hpp'...

---
+++ Tests/manyCalcs.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+
+#include <iostream>
+#include <string>
+
+#include "../LibreriasPersonalizadas/math_constants.hpp"
+#include "../LibreriasPersonalizadas/persTypes.hpp"
+
+using namespace std;
+
+string createLine(int size, char character);
+string createText(string text, int width);
+string createTextLine(string text, int totalWidth);
+void showMenu();
+
+int main() {}
